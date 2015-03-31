319d60259c8b74d19ef51ae7e487e495844ac144
PastLeo <chgu82837@gmail.com>
2015-03-31 11:42:32 +0800

    add some options

--- hw1_color_transfer/transColor.py
+++ hw1_color_transfer/transColor.py
@@ -11,13 +11,29 @@ import statistics as s
 
 if len(sys.argv) < 4:
     print("Usage:")
-    print("    python[3] transColor.py <source_img> <target_img> <destination_img> [dev_divider]")
+    print("    python[3] transColor.py [-OMbav] <source_img> <target_img> <destination_img>")
+    print("")
+    print("    [-OMbav] flags:")
+    print("    -O : disable log at convertion")
+    print("    -M : disable lms at convertion (Use pure RGB to convert) (Also disable log)")
+    print("    -b : use boundary to convert instead of statistics method")
+    print("    -a : adjust the result to source boundary")
+    print("    -v : show debug message")
     exit()
 
-if len(sys.argv) == 5:
-    dev_divider = float(sys.argv[4])
-else:
-    dev_divider = 2.5
+if sys.argv[1][0] == "-":
+    disable_log = "O" in sys.argv[1]
+    disable_lms = "M" in sys.argv[1]
+    use_boundary_method = "b" in sys.argv[1]
+    enable_boundary_adjust = "a" in sys.argv[1]
+    verbose = "v" in sys.argv[1]
+    sys.argv.pop(1)
+else: # Default flog value
+    disable_log = False
+    disable_lms = False
+    use_boundary_method = False
+    enable_boundary_adjust = False
+    verbose = False
 
 rgb2LMS = np.dot(
     np.array([0.3897,0.6890,-0.0787,-0.2298,1.1834,0.0464,0,0,1]).reshape(3,3),
@@ -31,22 +47,29 @@ LMS2lms = np.dot(
 lms2LMS = np.linalg.inv(LMS2lms)
 LMS2rgb = np.linalg.inv(rgb2LMS)
 
-# print("rgb2LMS",rgb2LMS)
-# print("LMS2lms",LMS2lms)
-# print("lms2LMS",lms2LMS)
-# print("LMS2rgb",LMS2rgb)
+if verbose:
+    print("Showing convertion matrix:")
+    print("rgb2LMS",rgb2LMS)
+    print("LMS2lms",LMS2lms)
+    print("lms2LMS",lms2LMS)
+    print("LMS2rgb",LMS2rgb)
 
-# print("lms2LMS * LMS2lms",np.dot(lms2LMS,LMS2lms))
-# print("LMS2rgb * rgb2LMS",np.dot(LMS2rgb,rgb2LMS))
+    print("lms2LMS * LMS2lms",np.dot(lms2LMS,LMS2lms))
+    print("LMS2rgb * rgb2LMS",np.dot(LMS2rgb,rgb2LMS))
 
-# exit()
+    # exit()
+
+log_min = m.log(1/255,10)
 
 def get_lms(img):
     img_ary = np.array(img)
     tmp = []
     avg = [0,0,0]
     dev = [0,0,0]
-    # pix = [[],[],[]]
+    max_v = [-100,-100,-100]
+    min_v = [100,100,100]
+    log_too_small = 0
+
     for y in range(img.size[0]):
         row = []
         for x in range(img.size[1]):
@@ -54,28 +77,34 @@ def get_lms(img):
             for e in np.nditer(img_ary[x,y,0:3]):
                 tmp_p.append(float(e) / 255)
             tmp_p = np.array(tmp_p)
-            # print(tmp_p)
-            tmp_p = np.dot(rgb2LMS,tmp_p)
-            # print(tmp_p)
-            for i in [0,1,2]:
-                tmp_p[i] = m.log(tmp_p[i],10)
-            # print(tmp_p)
-            tmp_p = np.dot(LMS2lms,tmp_p)
-            # print(tmp_p)
+
+            if not disable_lms:
+                tmp_p = np.dot(rgb2LMS,tmp_p)
+
+                if not disable_log:
+                    for i in [0,1,2]:
+                        if tmp_p[i] <= 0:
+                            if verbose:
+                                print("input too small:",tmp_p[i],"at",{'x':x,'y':y,'i':i})
+                            log_too_small += 1
+                            tmp_p[i] = log_min
+                        else:
+                            tmp_p[i] = m.log(tmp_p[i],10)
+                    tmp_p = np.dot(LMS2lms,tmp_p)
+
             for i in [0,1,2]:
                 avg[i] = avg[i] + tmp_p[i]
-                # pix[i].append(tmp_p[i])
-            # exit()
+                max_v[i] = max(max_v[i],tmp_p[i])
+                min_v[i] = min(min_v[i],tmp_p[i])
 
             row.append(tmp_p)
         tmp.append(row)
 
+    print("There are %d pixels that value is underflow (<= 0) when doing log, use value %f instead" % (log_too_small,log_min))
+
     pix_cnt = src_i.size[0] * src_i.size[1]
     for i in [0,1,2]:
         avg[i] = avg[i] / pix_cnt
-
-    # print("pix_cnt",pix_cnt)
-    # print("avg",avg,[s.mean(cha) for cha in pix])
 
     for row in tmp:
         for tmp_p in row:
@@ -85,22 +114,23 @@ def get_lms(img):
     for i in [0,1,2]:
         dev[i] = m.sqrt(dev[i] / pix_cnt)
 
-    # print("dev",dev,[s.pstdev(cha) for cha in pix])
-    # exit()
-
-    return tmp,avg,dev
+    return tmp,avg,dev,max_v,min_v
 
 print("Starting to get infomation of source image...")
 src_i = Image.open(sys.argv[1])
-src,src_avg,src_dev = get_lms(src_i)
+src,src_avg,src_dev,src_max,src_min = get_lms(src_i)
 print("src_avg",src_avg)
 print("src_dev",src_dev)
+print("src_max",src_max)
+print("src_min",src_min)
 
 print("Starting to get infomation of target image...")
 tar_i = Image.open(sys.argv[2])
-tar,tar_avg,tar_dev = get_lms(tar_i)
+tar,tar_avg,tar_dev,tar_max,tar_min = get_lms(tar_i)
 print("tar_avg",tar_avg)
 print("tar_dev",tar_dev)
+print("tar_max",tar_max)
+print("tar_min",tar_min)
 
 # exit()
 
@@ -109,20 +139,100 @@ print("Starting to process the destination image...")
 des_i = Image.new("RGB",src_i.size)
 des = np.array(des_i)
 
+des_tmp = []
+
+if enable_boundary_adjust:
+    res_max = [-100,-100,-100]
+    res_min = [100,100,100]
+
+if use_boundary_method: # ================= boundary method ======================
+    print("Using boundary method ...")
+    inte_ratio = [0]
+
+    for i in [1,2]:
+        inte_ratio.append((tar_max[i] - tar_min[i]) / (src_max[i] - src_min[i]))
+
+    print("inte_ratio:",inte_ratio)
+
+    for y in range(src_i.size[0]):
+        des_r_tmp = []
+        for x in range(src_i.size[1]):
+            tmp = [src[y][x][0],0,0]
+            for i in [1,2]:
+                tmp[i] = tar_min[i] + (src[y][x][i] - src_min[i]) * inte_ratio[i]
+
+                if enable_boundary_adjust:
+                    res_max[i] = max(res_max[i],tmp[i])
+                    res_min[i] = min(res_min[i],tmp[i])
+            des_r_tmp.append(tmp)
+        des_tmp.append(des_r_tmp)
+
+else: # ================== statistics method ======================
+    scale = []
+    for i in [0,1,2]:
+        scale.append(tar_dev[i] / src_dev[i])# / dev_divider
+    
+    print("Using statistics method ...")
+    for y in range(src_i.size[0]):
+        des_r_tmp = []
+        for x in range(src_i.size[1]):
+            tmp = [0,0,0]
+            for i in [0,1,2]:
+                # tmp[i] = src[y][x][i]
+                tmp[i] = (src[y][x][i] - src_avg[i]) * scale[i] + src_avg[i]
+
+                if enable_boundary_adjust:
+                    res_max[i] = max(res_max[i],tmp[i])
+                    res_min[i] = min(res_min[i],tmp[i])
+            des_r_tmp.append(tmp)
+        des_tmp.append(des_r_tmp)
+# ================== convertion done ======================
+
+if enable_boundary_adjust:
+    print("res_max:",res_max)
+    print("res_min:",res_min)
+
+if enable_boundary_adjust:
+    print("Starting to adjust result boundary to source boundary ...")
+    adj_inte_ratio = []
+    for i in [0,1,2]:
+        adj_inte_ratio.append((src_max[i] - src_min[i]) / (res_max[i] - res_min[i]))
+    for y in range(src_i.size[0]):
+        for x in range(src_i.size[1]):
+            for i in [0,1,2]:
+                des_tmp[y][x][i] = src_min[i] + (des_tmp[y][x][i] - res_min[i]) * adj_inte_ratio[i]
+
+# ================= convert back =========================
+
+too_big = 0
+too_small = 0
+
 for y in range(src_i.size[0]):
     for x in range(src_i.size[1]):
-        tmp = [0,0,0]
+        tmp = des_tmp[y][x]
+
+        if not disable_lms:
+            if not disable_log:
+                tmp = np.dot(lms2LMS,tmp)
+                for i in [0,1,2]:
+                    tmp[i] = m.pow(10,tmp[i])
+            tmp = np.dot(LMS2rgb,tmp)
         for i in [0,1,2]:
-            # tmp[i] = src[y][x][i]
-            scale = tar_dev[i] / src_dev[i] / dev_divider
-            tmp[i] = (src[y][x][i] - src_avg[i]) * scale + src_avg[i]
-        tmp = np.dot(lms2LMS,tmp)
-        for i in [0,1,2]:
-            tmp[i] = m.pow(10,tmp[i])
-        tmp = np.dot(LMS2rgb,tmp)
-        for i in [0,1,2]:
+            if tmp[i] > 1:
+                if verbose:
+                    print("final result too big:",tmp[i],"at",{'x':x,'y':y,'i':i},"... Auto fix to 1")
+                too_big += 1
+                tmp[i] = 1
+            if tmp[i] < 0:
+                if verbose:
+                    print("final result too small:",tmp[i],"at",{'x':x,'y':y,'i':i},"... Auto fix to 0")
+                too_small += 1
+                tmp[i] = 0
             des[x,y,i] = int(tmp[i] * 255)
         # print(des[x,y])
+
+print("There are %d pixels in result that value is overflow ... Auto fix to 1" % too_big)
+print("There are %d pixels in result that value is underflow ... Auto fix to 0" % too_small)
 
 # exit()
 Image.fromarray(des).save(sys.argv[3])
